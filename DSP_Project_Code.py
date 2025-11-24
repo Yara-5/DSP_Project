af6befac8a9d0e9d43929bd566a228536aa5469c
Yara-5 <yara2005@aucegypt.edu>
2025-11-25 01:23:39 +0200

    Initial commit

---
+++ DSP_Project_Code.py
@@ -0,0 +1,34 @@
+
+import scipy.io as sio
+
+
+def process_50Hz ():
+    pass
+
+def feature_1():
+    pass
+
+def feature_2():
+    pass
+
+def feature_3():
+    pass
+
+def feature_4():
+    pass
+
+def feature_5():
+    pass
+
+def classify_80_20():
+    pass
+
+def classify_KNN():
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
